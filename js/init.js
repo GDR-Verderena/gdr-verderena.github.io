4fb4483ab888feb817810f910b1c81efb092d206
João Gomes <joaosilgo96@gmail.com>
2020-09-03 00:27:51 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,4 @@
-(function($){
-  $(function(){
-
+  $(document).ready(function(){
     $('.sidenav').sidenav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+    
