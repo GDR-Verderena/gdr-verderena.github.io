f5bc5861be0fed8849bfa3a296e8f903ac75cc51
João Gomes <joaosilgo96@gmail.com>
2020-09-03 05:21:59 +0100

    Update and rename init.js to index.js

--- js/init.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const M = window.M;
-  var elems = document.querySelectorAll('.sidenav');
-  var opts = {
-    edge: 'left' // Choose the horizontal origin
-  };
-  var instances = M.Sidenav.init(elems, opts);
-});
