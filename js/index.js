0b87d4967ee7f911ae3cee0134c581ec3049c5bd
João Gomes <joaosilgo96@gmail.com>
2020-09-04 02:02:30 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-// content will be appended to .container
-$('.timeline').infiniteScroll({
-  // options...
-  elementScroll: true,
-  // use .container overflow element scrolling
-});
+document.querySelector("timeline").style.height = "1000px";
+
+window.addEventListener("scroll", function() {
+    var body = document.querySelector("timeline");
+    var height = body.style.height;
+    height = height.slice(0, -2);
+    height = Number(height);
+    return function() {
+        if(height - window.scrollY < 700) {
+            height += height / 2;
+        }
+        body.style.height = height + "px";
+    };
+}());
