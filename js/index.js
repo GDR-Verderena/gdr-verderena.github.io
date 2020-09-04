329a4aef29c69883a1b1bdec8ad927eeaff356b0
João Gomes <joaosilgo96@gmail.com>
2020-09-04 02:33:43 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(window).on("scroll", function () {
-    const scrollHeight = $(document).height() as number;
+    const scrollHeight = $(document).height() ;
     const scrollPos = Math.floor($(window).height() + $(window).scrollTop());
     if (scrollHeight === scrollPos) {
         console.log("Scroll MF!");
