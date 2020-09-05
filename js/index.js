4fc39f0fad37ae14da7046d98a571272478453cb
João Gomes <joaosilgo96@gmail.com>
2020-09-06 00:58:05 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
   AOS.init();
+
+
+
+function imageExists(url, callback) {
+  var img = new Image();
+  img.onload = function () { callback(true); };
+  img.onerror = function () { callback(false); };
+  img.src = url;
+}
+
+
+function imageValidate(url) {
+  imageExists(url, function (value) {
+    //Show the result
+    console.log(value);
+    return value;
+  });
+ }
+
