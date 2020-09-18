c68be1b5e9bd756e008e2f003b5d968e25993124
João Gomes <joaosilgo96@gmail.com>
2020-09-18 05:14:37 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@
 
 
 $(function(){
-    $(".post").slice(0, 2).show(); // select the first ten
+    $(".post").slice(0, 5).show(); // select the first 5
     $("#load").click(function(e){ // click event for load more
         e.preventDefault();
-        $(".post:hidden").slice(0, 2).show(); // select next 10 hidden divs and show them
-        if($(".post:hidden").length == 0){ // check if any hidden divs still exist
-           $("#load").hide()
+        $(".post:hidden").slice(0, 5).show(); // select next 5 hidden posts and show them
+        if($(".post:hidden").length == 0){ // check if any hidden ppost still exist
+          
+		$("#load").hide(); // hide the button
+		
            // alert("No more divs"); // alert if there are none left
         }
     });
