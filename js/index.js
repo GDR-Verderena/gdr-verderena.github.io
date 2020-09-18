fefacefa121bc7fa7d0bee0e39adef2ad0afb108
João Gomes <joaosilgo96@gmail.com>
2020-09-18 03:25:16 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
   AOS.init();
 
 
+$(function(){
+    $(".post").slice(0, 2).show(); // select the first ten
+    $("#load").click(function(e){ // click event for load more
+        e.preventDefault();
+        $(".post:hidden").slice(0, 2).show(); // select next 10 hidden divs and show them
+        if($(".post:hidden").length == 0){ // check if any hidden divs still exist
+           $("#load").hide()
+           // alert("No more divs"); // alert if there are none left
+        }
+    });
+});
+
+
+
+
+
 
 
 var mybutton = document.getElementById("myBtn");
