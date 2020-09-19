f703e6a14855cece8ced1e91db8d26b395bc5e5d
João Gomes <joaosilgo96@gmail.com>
2020-09-19 04:07:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -153,7 +153,33 @@ self.addEventListener('fetch', function (event) {
         })
     );
 });
+ 
+ 
+ 
+ 
+ 
+ self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 
+ 
+
+ 
+ 
+ 
+ 
  
  
 if ('storage' in navigator && 'estimate' in navigator.storage) {
