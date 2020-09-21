fa0bc91f95f77121c0f55d98cebf5447c7174e86
João Gomes <joaosilgo96@gmail.com>
2020-09-21 20:20:49 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
   AOS.init();
 
 
+!(function($) {
+  "use strict";
+	
+	
+	
+  // Intro carousel
+  var heroCarousel = $("#heroCarousel");
+  var heroCarouselIndicators = $("#hero-carousel-indicators");
+  heroCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    (index === 0) ?
+    heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>"):
+      heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
+
+    $(this).css("background-image", "url('" + $(this).children('.carousel-background').children('img').attr('src') + "')");
+    $(this).children('.carousel-background').remove();
+  });
+
+  heroCarousel.on('slid.bs.carousel', function(e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
+  });
+})(jQuery);
+
+
+
 //$(img).attr(draggable: false);
 
 if ('storage' in navigator && 'estimate' in navigator.storage) {
