e1f6576302943f2d8a23b7e625789f18bef2b687
João Gomes <joaosilgo96@gmail.com>
2020-09-28 21:45:53 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -42,6 +42,14 @@ function showNotification() {
   });
 	
 
+	
+	
+	notification.onclick = function(event) {
+  event.preventDefault(); // prevent the browser from focusing the Notification's tab
+  window.open('http://www.mozilla.org', '_blank');
+}
+	
+	
 }
 
 
