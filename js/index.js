ec6be7b6777158ec5b48ed176c4df28d37593a07
João Gomes <joaosilgo96@gmail.com>
2020-09-29 00:45:46 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -78,79 +78,7 @@ if ('actions' in Notification.prototype) {
 //Teste
 
 
-function doSomething() {
 
-
-
-
-const title = 'Actions Notification';
-  const options = {
-    actions: [
-      {
-        action: 'coffee-action',
-        title: 'Coffee'
-       
-      },
-      {
-        action: 'doughnut-action',
-        title: 'Doughnut'
-        
-      },
-      {
-        action: 'gramophone-action',
-        title: 'gramophone'
-       
-      },
-      {
-        action: 'atom-action',
-        title: 'Atom'
-       
-      }
-    ]
-  };
-
-  const maxVisibleActions = Notification.maxActions;
-  if (maxVisibleActions < 4) {
-    options.body = `This notification will only display ` +
-      `${maxVisibleActions} actions.`;
-  } else {
-    options.body = `This notification can display up to ` +
-      `${maxVisibleActions} actions.`;
-  }
-
-  reg.showNotification(title, options);
-
-
-self.addEventListener('notificationclick', function(event) {
-  if (!event.action) {
-    // Was a normal notification click
-    console.log('Notification Click.');
-    return;
-  }
-
-  switch (event.action) {
-    case 'coffee-action':
-      console.log('User ❤️️\'s coffee.');
-      break;
-    case 'doughnut-action':
-      console.log('User ❤️️\'s doughnuts.');
-      break;
-    case 'gramophone-action':
-      console.log('User ❤️️\'s music.');
-      break;
-    case 'atom-action':
-      console.log('User ❤️️\'s science.');
-      break;
-    default:
-      console.log(`Unknown action clicked: '${event.action}'`);
-      break;
-  }
-});
-
-
-}
-
- doSomething();
 //Teste Finish
 
 
