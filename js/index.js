40cbbb1d59b82715f885947f3ad27eff5ff7004f
João Gomes <joaosilgo96@gmail.com>
2020-09-28 22:37:42 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,6 +47,35 @@ function showNotification() {
 }
 
 
+
+self.addEventListener('notificationclick', function(event) {
+  if (!event.action) {
+    // Was a normal notification click
+    console.log('Notification Click.');
+    return;
+  }
+
+  switch (event.action) {
+    case 'coffee-action':
+      console.log('User ❤️️\'s coffee.');
+      break;
+    case 'doughnut-action':
+      console.log('User ❤️️\'s doughnuts.');
+      break;
+    case 'gramophone-action':
+      console.log('User ❤️️\'s music.');
+      break;
+    case 'atom-action':
+      console.log('User ❤️️\'s science.');
+      break;
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }
+});
+
+
+/*
 function spawnNotification(theBody,  theTitle, theLink) {
   var options = {
     body: 'GDR Notification!',
@@ -68,6 +97,8 @@ function spawnNotification(theBody,  theTitle, theLink) {
 }
 
 spawnNotification('The Body', 'The Title', 'https://joaosilgo.github.io/BlazorApp/');
+
+*/
 
 
 
