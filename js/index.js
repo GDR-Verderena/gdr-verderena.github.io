8e0afdb3d0d573d5da41cc78fd141a2b6e1751e1
João Gomes <joaosilgo96@gmail.com>
2020-09-28 22:02:08 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -41,16 +41,28 @@ function showNotification() {
     }
   });
 	
+	
+	
+}
 
-	
-	
-	Notification.onclick = function(event) {
-  event.preventDefault(); // prevent the browser from focusing the Notification's tab
-  window.open('http://www.mozilla.org', '_blank');
+
+function spawnNotification(theBody, theIcon, theTitle, theLink) {
+  var options = {
+    body: theBody,
+    icon: theIcon
+  }
+  var notification = new Notification(theTitle, options);
+  notification.onclick = function(event) {
+    event.preventDefault(); // prevent the browser from focusing the Notification's tab
+    window.open(theLink, '_blank');
+  }
+
+  setTimeout(notification.close.bind(notification), 7000);
 }
-	
-	
-}
+
+spawnNotification();
+
+
 
 
 
