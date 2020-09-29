3e47a6bd637531e6acdefdba8a4189e6b87e65d3
João Gomes <joaosilgo96@gmail.com>
2020-09-29 02:52:40 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -56,14 +56,24 @@ function showNotification() {
 
 
 
-navigator.serviceWorker.addEventListener('notificationclick', function(event) {
+self.onnotificationclick = function(event) {
+  console.log('On notification click: ', event.notification.tag);
   event.notification.close();
-  if (event.action === 'coffee-action') {
-    console.log('Website');
-  } else {
-    clients.openWindow("/about");
-  }
-}, false);
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+};
 
 
 
