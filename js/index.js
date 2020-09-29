528f4bfbb1b456ffccfbbcbb601ce9523fd1d3ec
João Gomes <joaosilgo96@gmail.com>
2020-09-29 01:42:15 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,14 @@
 
 
 
+let magSensor = new Magnetometer({frequency: 60});
 
+magSensor.addEventListener('reading', e => {
+  console.log("Magnetic field along the X-axis " + magSensor.x);
+  console.log("Magnetic field along the Y-axis " + magSensor.y);
+  console.log("Magnetic field along the Z-axis " + magSensor.z);
+});
+magSensor.start();
 
 
 
