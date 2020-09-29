1f2fb9ebb822cd490ede731176c65b237a47f571
João Gomes <joaosilgo96@gmail.com>
2020-09-29 04:44:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -199,3 +199,37 @@ if ('storage' in navigator && 'estimate' in navigator.storage) {
   });
 }
 */
+
+ 
+ 
+ 
+ 
+ 
+ 
+ /**** START notificationActionClickEvent ****/
+self.addEventListener('notificationclick', function(event) {
+  if (!event.action) {
+    // Was a normal notification click
+    console.log('Notification Click.');
+    return;
+  }
+
+  switch (event.action) {
+    case 'coffee-action':
+      console.log('User ❤️️\'s coffee.');
+      break;
+    case 'doughnut-action':
+      console.log('User ❤️️\'s doughnuts.');
+      break;
+    case 'gramophone-action':
+      console.log('User ❤️️\'s music.');
+      break;
+    case 'atom-action':
+      console.log('User ❤️️\'s science.');
+      break;
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }
+});
+/**** END notificationActionClickEvent ****/
