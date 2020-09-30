dcc761378d4589b571217b628fc3d0a04e22d731
João Gomes <joaosilgo96@gmail.com>
2020-09-30 03:36:25 +0100

    Create app-install.js

---
+++ js/app-install.js
@@ -0,0 +1,37 @@
+"use strict";
+
+(function (app) {
+  const butInstall = document.getElementById("butInstall");
+
+  /**
+   * Track successful app installs
+   */
+  window.addEventListener("appinstalled", (e) => {
+    console.log(installed);
+  });
+
+  /**
+   * Listen for 'beforeinstallprompt' event, and update the UI to indicate
+   * app can be installed.
+   */
+  window.addEventListener("beforeinstallprompt", (e) => {
+    // Don't show the mini-info bar
+    e.preventDefault();
+
+    // Log that install is available.
+
+    // Save the deferred prompt
+    app.installPrompt = e;
+
+    // Show the install button
+    butInstall.removeAttribute("disabled");
+    butInstall.classList.remove("hidden");
+  });
+
+  // Handle the install button click
+  butInstall.addEventListener("click", () => {
+    butInstall.setAttribute("disabled", true);
+    app.installPrompt.prompt();
+    console.log("Install clicked");
+  });
+})(app);
