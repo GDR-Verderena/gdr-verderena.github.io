36fc7b6326a5759c61edb2b8769a6ffac4c1d88f
João Gomes <joaosilgo96@gmail.com>
2020-09-30 17:34:37 +0100

    Update indexedDB.js

--- js/indexedDB.js
+++ js/indexedDB.js
@@ -25,3 +25,21 @@ window.onload = function() {
     // Run the displayData() function to display the notes already in the IDB
   //  displayData();
   };
+  
+  
+   // Setup the database tables if this has not already been done
+  request.onupgradeneeded = function(e) {
+
+    // Grab a reference to the opened database
+    let db = e.target.result;
+
+    // Create an objectStore to store our notes in (basically like a single table)
+    // including a auto-incrementing key
+    let objectStore = db.createObjectStore('notes_os', { keyPath: 'id', autoIncrement:true });
+
+    // Define what data items the objectStore will contain
+    objectStore.createIndex('title', 'title', { unique: false });
+    objectStore.createIndex('body', 'body', { unique: false });
+
+    console.log('Database setup complete');
+  };
