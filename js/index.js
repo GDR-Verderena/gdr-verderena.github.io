9c185a36c2127a74c483c135ba8da12e7b2f2b04
João Gomes <joaosilgo96@gmail.com>
2020-10-01 21:54:50 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -463,21 +463,6 @@ if ("storage" in navigator && "estimate" in navigator.storage) {
   });
 }
 
-$(function () {
-  $(".post").slice(0, 5).show(); // select the first 5
-  $("#load").click(function (e) {
-    // click event for load more
-    e.preventDefault();
-    $(".post:hidden").slice(0, 5).show(); // select next 5 hidden posts and show them
-    if ($(".post:hidden").length == 0) {
-      // check if any hidden ppost still exist
-
-      $("#load").hide(); // hide the button
-
-      // alert("No more divs"); // alert if there are none left
-    }
-  });
-});
 
 var mybutton = document.getElementById("myBtn");
 
