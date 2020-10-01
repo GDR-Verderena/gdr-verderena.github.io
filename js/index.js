32aca721f8fb24bcbf4a372ccbc8e4bee28e53fa
João Gomes <joaosilgo96@gmail.com>
2020-10-01 21:27:33 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -35,6 +35,33 @@ function myFunction() {
 
 
 
+
+if(window.localStorage){
+var formValues = JSON.parse(localStorage.getItem("formValues")) || {};
+var $checkboxes = $("#checkbox-container :checkbox");
+
+function updateStorage() {
+  $checkboxes.each(function () {
+    formValues[this.id] = this.checked;
+  });
+
+  localStorage.setItem("formValues", JSON.stringify(formValues));
+}
+
+$checkboxes.on("change", function () {
+  updateStorage();
+});
+
+// On page load
+$.each(formValues, function (key, value) {
+  $("#" + key).prop("checked", value);
+});
+}
+
+myFunction();
+
+
+
 /*
 
 if ("showTrigger" in Notification.prototype) {
@@ -420,30 +447,6 @@ if ('periodicSync' in registration) {
 
 
 
-
-if(window.localStorage){
-var formValues = JSON.parse(localStorage.getItem("formValues")) || {};
-var $checkboxes = $("#checkbox-container :checkbox");
-
-function updateStorage() {
-  $checkboxes.each(function () {
-    formValues[this.id] = this.checked;
-  });
-
-  localStorage.setItem("formValues", JSON.stringify(formValues));
-}
-
-$checkboxes.on("change", function () {
-  updateStorage();
-});
-
-// On page load
-$.each(formValues, function (key, value) {
-  $("#" + key).prop("checked", value);
-});
-}
-
-myFunction();
 
 !(function ($) {
   "use strict";
