6aff034aff1ecf1639437adadda13664fb0297e4
João Gomes <joaosilgo96@gmail.com>
2020-10-01 22:16:25 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,36 +17,31 @@ $(function () {
   });
 });
 
+if (window.localStorage) {
+  var formValues = JSON.parse(localStorage.getItem("formValues")) || {};
+  var $checkboxes = $("#checkbox-container :checkbox");
 
+  function updateStorage() {
+    $checkboxes.each(function () {
+      formValues[this.id] = this.checked;
+    });
 
-if(window.localStorage){
-var formValues = JSON.parse(localStorage.getItem("formValues")) || {};
-var $checkboxes = $("#checkbox-container :checkbox");
+    localStorage.setItem("formValues", JSON.stringify(formValues));
+  }
 
-function updateStorage() {
-  $checkboxes.each(function () {
-    formValues[this.id] = this.checked;
+  $checkboxes.on("change", function () {
+    updateStorage();
   });
 
-  localStorage.setItem("formValues", JSON.stringify(formValues));
-}
-
-$checkboxes.on("change", function () {
-  updateStorage();
-});
-
-// On page load
-$.each(formValues, function (key, value) {
-  $("#" + key).prop("checked", value);
-});
+  // On page load
+  $.each(formValues, function (key, value) {
+    $("#" + key).prop("checked", value);
+  });
 }
 
 myFunction();
 
-
-
 /*
-
 if ("showTrigger" in Notification.prototype) {
   console.log('showTrigger is Enable');
 }
@@ -55,46 +50,29 @@ else{
 }
 */
 
-
 /*
-
 registerNotification();
-
-
-
 function registerNotification() {
 	Notification.requestPermission(permission => {
 		if (permission === 'granted'){ registerBackgroundSync() }
 		else console.error("Permission was not granted.")
 	})
 }
-
-
-
 function registerBackgroundSync() {
     if (!navigator.serviceWorker){
         return console.error("Service Worker not supported")
     }
-
     navigator.serviceWorker.ready
     .then(registration => registration.sync.register('syncAttendees'))
     .then(() => console.log("Registered background sync"))
     .catch(err => console.error("Error registering background sync", err))
 }
-
-
 self.addEventListener('sync', function(event) {
 	console.log("sync event", event);
     if (event.tag === 'syncAttendees') {
         event.waitUntil(syncAttendees()); // sending sync request
     }
 });
-
-
-
-
-
-
 function syncAttendees(){
 	return update({ url: `https://gdrverderena.pt/blog/atom.xml` })
     	.then(refresh)
@@ -102,176 +80,163 @@ function syncAttendees(){
     		`${attendees.length} attendees to the PWA Workshop`
     	))
 }
-
-
-
 */
-
 
 /* Test */
 
-if ("Notification" in window  && "Notification" in navigator ) {
-	function getSW() {
-		return navigator.serviceWorker.getRegistration("/sw.js");
-	}
+if ("Notification" in window && "Notification" in navigator) {
+  function getSW() {
+    return navigator.serviceWorker.getRegistration("/sw.js");
+  }
 
-	async function onActionsClick() {
-		const reg = await getSW();
-		/**** START actionsNotification ****/
-		const title = "GDR Verderena Newsletter";
-		const subscriberEmail = document.querySelector("#subscriberEmail").value;
-		const options = {
-			actions: [
-				{
-					action: "coffee-action",
-					title: "Website",
-					icon:
-						"https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
-				},
-				{
-					action: "doughnut-action",
-					title: "Facebook",
-					icon:
-						"https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
-				},
-				{
-					action: "gramophone-action",
-					title: "gramophone",
-					icon:
-						"https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
-				},
-				{
-					action: "atom-action",
-					title: "Atom",
-					icon:
-						"https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
-				}
-			],
+  async function onActionsClick() {
+    const reg = await getSW();
+    /**** START actionsNotification ****/
+    const title = "GDR Verderena Newsletter";
+    const subscriberEmail = document.querySelector("#subscriberEmail").value;
+    const options = {
+      actions: [
+        {
+          action: "coffee-action",
+          title: "Website",
+          icon:
+            "https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
+        },
+        {
+          action: "doughnut-action",
+          title: "Facebook",
+          icon:
+            "https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
+        },
+        {
+          action: "gramophone-action",
+          title: "gramophone",
+          icon:
+            "https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
+        },
+        {
+          action: "atom-action",
+          title: "Atom",
+          icon:
+            "https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
+        }
+      ],
 
-			body:
-				"Psst! Psst!" +
-				"\n" +
-				subscriberEmail +
-				"\nNão percas as Novidades de GDR Verderena 🎯",
-			lang: "pt-PT",
-			vibrate: [
-				100,
-				200,
-				100,
-				200,
-				100,
-				200,
-				100,
-				200,
-				100,
-				100,
-				100,
-				100,
-				100,
-				200,
-				100,
-				200,
-				100,
-				200,
-				100,
-				200,
-				100,
-				100,
-				100,
-				100,
-				100,
-				200,
-				100,
-				200,
-				100,
-				200,
-				100,
-				200,
-				100,
-				100,
-				100,
-				100,
-				100,
-				100,
-				100,
-				100,
-				100,
-				100,
-				50,
-				50,
-				100,
-				800
-			],
-			tag: "Newsletter",
-			renotify: true,
-			icon:
-				"https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png",
-			data: {
-				createdAt: new Date(Date.now()).toString(),
-				message: "Hello, World!",
-				dateOfArrival: Date.now(),
-				primaryKey: 1
-			},
-			dir: "auto",
-			requireInteraction: true,
-			badge:
-				"https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
-		};
+      body:
+        "Psst! Psst!" +
+        "\n" +
+        subscriberEmail +
+        "\nNão percas as Novidades de GDR Verderena 🎯",
+      lang: "pt-PT",
+      vibrate: [
+        100,
+        200,
+        100,
+        200,
+        100,
+        200,
+        100,
+        200,
+        100,
+        100,
+        100,
+        100,
+        100,
+        200,
+        100,
+        200,
+        100,
+        200,
+        100,
+        200,
+        100,
+        100,
+        100,
+        100,
+        100,
+        200,
+        100,
+        200,
+        100,
+        200,
+        100,
+        200,
+        100,
+        100,
+        100,
+        100,
+        100,
+        100,
+        100,
+        100,
+        100,
+        100,
+        50,
+        50,
+        100,
+        800
+      ],
+      tag: "Newsletter",
+      renotify: true,
+      icon:
+        "https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png",
+      data: {
+        createdAt: new Date(Date.now()).toString(),
+        message: "Hello, World!",
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      },
+      dir: "auto",
+      requireInteraction: true,
+      badge:
+        "https://raw.githubusercontent.com/GDR-Verderena/gdr-verderena.github.io/master/assets/img/badge-128x128.png"
+    };
 
-		Notification.requestPermission(function (result) {
-			if (result === "granted") {
-				reg.showNotification(title, options);
-			}
-		});
-		/**** END actionsNotification ****/
-	}
+    Notification.requestPermission(function (result) {
+      if (result === "granted") {
+        reg.showNotification(title, options);
+      }
+    });
+    /**** END actionsNotification ****/
+  }
 
-	function isActionsSupported() {
-		return "actions" in Notification.prototype;
-	}
+  function isActionsSupported() {
+    return "actions" in Notification.prototype;
+  }
 
-	window.addEventListener("load", () => {
-		if (!isActionsSupported()) {
-			return;
-		}
+  window.addEventListener("load", () => {
+    if (!isActionsSupported()) {
+      return;
+    }
 
-		const btn = document.querySelector(".js-notification-actions");
-		const form = document.querySelector("#subscribe");
-		btn.disabled = false;
-		// btn.addEventListener("click", onActionsClick);
+    const btn = document.querySelector(".js-notification-actions");
+    const form = document.querySelector("#subscribe");
+    btn.disabled = false;
+    // btn.addEventListener("click", onActionsClick);
 
-		form.addEventListener("submit", function (evt) {
-			evt.preventDefault();
-			onActionsClick();
-		});
-	});
+    form.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+      onActionsClick();
+    });
+  });
 }
 
-
-
-
-if (navigator.vibrate) { 
-	//navigator.vibrate(1000); 
-	inputs = document.querySelector("#subscriberEmail"); 
-	inputs.addEventListener('invalid', function(e) { 
-		console.log('vibrate'); 
-		window.navigator.vibrate([100, 50, 100]); 
-	},  false  ); 
+if (navigator.vibrate) {
+  //navigator.vibrate(1000);
+  inputs = document.querySelector("#subscriberEmail");
+  inputs.addEventListener(
+    "invalid",
+    function (e) {
+      console.log("vibrate");
+      window.navigator.vibrate([100, 50, 100]);
+    },
+    false
+  );
 }
-
-
-
-
-
-
-
-
 
 /**** Test ****/
 
-
 /*
-
 function showNotification() {
   Notification.requestPermission(function (result) {
     if (result === "granted") {
@@ -361,26 +326,13 @@ function showNotification() {
     }
   });
 }
-
 if ("actions" in Notification.prototype) {
   console.log("👌");
 } else {
   // Action buttons are NOT supported.
   console.log("👎");
 }
-
-
 //Teste
-
-
-
-
-
-
-
-
-
-
 const status =  navigator.permissions.query({
   name: 'periodic-background-sync',
 });
@@ -391,10 +343,6 @@ if (status.state === 'granted') {
   // Periodic background sync cannot be used.
 	console.log('Periodic background sync cannot be used.');	
 }
-
-
-
-
 const registration =  navigator.serviceWorker.ready;
 if ('periodicSync' in registration) {
   try {
@@ -407,29 +355,7 @@ if ('periodicSync' in registration) {
 	  console.log('Periodic background sync cannot be used.');
   }
 }
-
-
-
-
-
-
-
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 !(function ($) {
   "use strict";
@@ -478,7 +404,6 @@ if ("storage" in navigator && "estimate" in navigator.storage) {
   });
 }
 
-
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -520,43 +445,34 @@ imageValidate = function (url) {
 };
 
 /*
-
 	function filter(tag) {
   setActiveTag(tag);
   showContainer(tag);
 }
-
 function setActiveTag(tag) {
   // loop through all items and remove active class
   var items = document.getElementsByClassName('blog-tag-item');
   for(var i=0; i < items.length; i++) {
     items[i].setAttribute('class', 'blog-tag-item');
   }
-
   // set the selected tag's item to active
   var item = document.getElementById(tag + '-item');
   if(item) {
     item.setAttribute('class', 'blog-tag-item active');
   }
 }
-
 function showContainer(tag) {
   // loop through all lists and hide them
   var lists = document.getElementsByClassName('blog-list-container');
   for(var i=0; i < lists.length; i++) {
     lists[i].setAttribute('class', 'blog-list-container hidden');
   }
-
   // remove the hidden class from the list corresponding to the selected tag
   var list = document.getElementById(tag + '-container');
   if(list) {
     list.setAttribute('class', 'blog-list-container');
   }
 }
-
-
-
-
 */
 
 
