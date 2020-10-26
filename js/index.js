28f4f2da50dc1d6098ca3547d08cdf62b9334c21
João Gomes <joaosilgo96@gmail.com>
2020-10-26 01:46:30 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 
-
+*/
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
    })
 });
 
-*/
+
 
 
 /*
